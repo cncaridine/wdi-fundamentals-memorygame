21eb32213daf31f5d7484846b95e36c6c2291b39
Corin Caridine <corincaridine@corins-air.attlocal.net>
2018-11-29 14:22:57 -0600

    created game logic

--- memory_game/js/main.js
+++ memory_game/js/main.js
@@ -1,6 +1,28 @@
 // new code on Nov 27
-var cards = ["queen", "queen", "king", "king"];
+var cards = [
+{
+rank: "queen",
+suit: "hearts",
+cardImage: "images/queen-of-hearts.png"	
+},
+{
+rank: "queen",
+suit: "diamonds",
+cardImage: "images/queen-of-diamonds.png"	
+},
+{
+rank: "king",
+suit: "hearts",
+cardImage: "images/king-of-hearts.png"
+},
+{
+rank: "king",
+suit: "diamonds",
+cardImage: "images/king-of-diamonds.png"	
+}
+]
 var cardsInPlay = [];
+
 
 var checkForMatch = function () {
 	if (cardsInPlay[0] === cardsInPlay[1]) {
@@ -12,12 +34,12 @@ var checkForMatch = function () {
 
 var flipCard = function (cardId) {
   var cardOne = cards[cardId];
-cardsInPlay.push(cardOne);
-console.log("User flipped " + cardOne);
+  cardsInPlay.push(cards[cardId].rank);
+console.log("User flipped " + cards[cardId].rank);
+console.log("User flipped" + cards[cardId].cardImage);
+console.log("User flipped" + cards[cardId].suit);
 
 };
-
-cardsInPlay.push(cards);
 
 flipCard(0);
 flipCard(2);
